bd7a96379c4e3a444b9b1a3b19b7ebc17957892f
AdrianoRama <ramaadriano@outlook.com>
2022-03-22 02:47:47 +0100

    Co-authored-by: Visard123 <Visard123@users.noreply.github.com>

--- src/container/Login/Login.jsx
+++ src/container/Login/Login.jsx
@@ -6,10 +6,26 @@ export default function Login() {
         <div className='app__login'>
             <div className="app__login-container">
                 <div className="app__login-log">
-                    log
+                    <form action="">
+                        <div className="app__form">
+                            <h1 className="headtext__cormorant">Log In</h1>
+                            <input type="text" name='username' placeholder='username' required />
+                            <input type="password" name='password' placeholder='password' required />
+                            <button type='submit'>SUBMIT</button>
+                        </div>
+                    </form>
                 </div>
                 <div className="app__login-sign">
-                    sign
+                    <form action="">
+                        <div className="app__form">
+                            <h1 className="headtext__cormorant">Sign Up</h1>
+                            <input type="text" name='username' placeholder='username' required />
+                            <input type="text" name='name' placeholder='name' required />
+                            <input type="email" name='email' placeholder='email' required />
+                            <input type="password" name='password' placeholder='password' required />
+                            <button type='submit'>SUBMIT</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
